efb51d22d8412b75d25e80d1ed02f6a7af013e5d
itsparasbisht <iamparasbisht@gmail.com>
2023-03-25 14:51:29 +0530

    adding custom font family

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="border-4 border-reactBlue">Hello world!</h1>
-      <h1 className="text-red-500 bg-yellow-200">Hello world!</h1>
+      <h1 className="text-xs">title 1</h1>
+      <h2>title 2</h2>
+      <h3>title 3</h3>
+      <p className="text-2xl">Note this</p>
+      <p className="text-base">Note this</p>
     </div>
   );
 }
