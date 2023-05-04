b4a63ba48c60dd9e0b1d6a90617db927dc53451f
itsparasbisht <iamparasbisht@gmail.com>
2023-05-04 14:13:18 +0530

    TopBar component added elements

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { CiSearch } from "react-icons/ci";
+import { CiSearch, CiBellOn } from "react-icons/ci";
 import { BsFillMicFill } from "react-icons/bs";
+import { MdEmergencyRecording } from "react-icons/md";
 
 function Topbar() {
   return (
@@ -16,7 +17,7 @@ function Topbar() {
       <div className="w-1/2 h-full flex items-center">
         <input
           type="text"
-          className="w-[77%] h-[90%] py-2 px-4 rounded-tl-full rounded-bl-full bg-[#121212] border-gray-700 border-[0.5px] text-lg outline-none text-gray-300"
+          className="w-[77%] h-[90%] py-2 px-4 rounded-tl-full rounded-bl-full bg-[#121212] border-gray-700 border-[0.5px] text-lg outline-none text-gray-300 focus:border-blue-700"
           placeholder="Search"
         />
         <div className="w-[11%] h-[90%] bg-[#242424] flex justify-center items-center rounded-tr-full rounded-br-full border-gray-700 border-[0.5px]">
@@ -26,7 +27,17 @@ function Topbar() {
           <BsFillMicFill className="text-xl text-white" />
         </div>
       </div>
-      <div className="w-1/4 bg-pink-200"></div>
+      <div className="w-1/4 h-full flex items-center justify-center">
+        <div className="w-10 h-10 bg-[#242424] flex justify-center items-center rounded-full ml-4">
+          <MdEmergencyRecording className="text-2xl text-white" />
+        </div>
+        <div className="w-10 h-10 bg-[#242424] flex justify-center items-center rounded-full ml-4">
+          <CiBellOn className="text-2xl text-white" />
+        </div>
+        <div className="w-10 h-10 bg-green-300 flex justify-center items-center rounded-full ml-4">
+          <span className="text-black text-xl">PB</span>
+        </div>
+      </div>
     </div>
   );
 }
